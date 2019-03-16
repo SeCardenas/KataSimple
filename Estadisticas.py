72adeccd91a430f1a539a23ffe09043c41e705cd
Oscar Espinel <oscaresp.194@gmail.com>
2019-03-15 22:02:08 -0500

    Iteración 1, Ciclo 3. Codigo para un string de dos numeros (Verde)

--- Estadisticas.py
+++ Estadisticas.py
@@ -3,5 +3,7 @@ class Estadisticas:
     def dar_estadisticas(self, cadena):
         if cadena=="":
             return [0]
+        elif "," in cadena:
+            return [2]
         else:
             return [1]
