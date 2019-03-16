501d1c883bb49a84762a98aed98e9cf6e31b95e5
Sergio Cárdenas <se.cardenas@uniandes.edu.co>
2019-03-15 22:05:17 -0500

    Iteración 1, ciclo 4. Test para n números (rojo)

--- EstadisticasTest.py
+++ EstadisticasTest.py
@@ -11,3 +11,7 @@ class TestEstadisticas(unittest.TestCase):
 
     def test_dar_estadiciticas_dos_numeros(self):
         self.assertEqual(Estadisticas().dar_estadisticas("1,2")[0], 2, "Dos numeros")
+
+    def test_dar_estadiciticas_n_numeros(self):
+        self.assertEqual(Estadisticas().dar_estadisticas("1,2,4,3")[0], 4, "N numeros")
+        self.assertEqual(Estadisticas().dar_estadisticas("1,2,7,5,6,1")[0], 6, "N numeros")
