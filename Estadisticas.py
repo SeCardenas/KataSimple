5657f9530faef863f682f736bd536e78b7a53e93
Oscar Espinel <oscaresp.194@gmail.com>
2019-03-15 23:20:48 -0500

    Iteración 4, Ciclo 3. Codigo para dos numeros (Verde)

--- Estadisticas.py
+++ Estadisticas.py
@@ -5,11 +5,13 @@ class Estadisticas:
             return [0,0,0,0]
         elif "," in cadena:
             numeros = cadena.split(",")
+            suma=0.0
             minimo = int(numeros[0])
             maximo = int(numeros[0])
             for numero in numeros:
                 minimo = min(int(numero),minimo)
                 maximo = max(int(numero),maximo)
-            return [len(numeros), minimo, maximo]
+                suma=suma+int(numero)
+            return [len(numeros), minimo, maximo, suma/len(numeros)]
         else:
             return [1,int(cadena), int(cadena), int(cadena)]
