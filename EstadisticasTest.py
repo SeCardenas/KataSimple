0a4e840ddb7cbefef5b44c1e3cbeb5680b086a10
Oscar Espinel <oscaresp.194@gmail.com>
2019-03-15 22:49:52 -0500

    Iteración 3, Ciclo 1. Codigo refactor para un string vacio (Azul)

--- EstadisticasTest.py
+++ EstadisticasTest.py
@@ -3,10 +3,6 @@ import unittest
 from Estadisticas import Estadisticas
 
 class TestEstadisticas(unittest.TestCase):
-    def test_dar_estadiciticas_string_vacio_y_minimo(self):
-        self.assertEqual(Estadisticas().dar_estadisticas("")[0], 0, "String vacio")
-        self.assertEqual(Estadisticas().dar_estadisticas("")[1], 0, "Minimo")
-
     def test_dar_estadiciticas_un_string_y_minimo(self):
         self.assertEqual(Estadisticas().dar_estadisticas("1")[0], 1, "Un solo numero")
         self.assertEqual(Estadisticas().dar_estadisticas("1")[1], 1, "Minimo")
