36a74453068f43b33b33ce635a73002bf14f306b
dtmaciasca <dt.macias@uniandes.edu.ci>
2019-03-15 22:15:17 -0500

    Iteracion 2. Ciclo 1. Se hace el test para un string vacio (Rojo)

--- EstadisticasTest.py
+++ EstadisticasTest.py
@@ -15,3 +15,7 @@ class TestEstadisticas(unittest.TestCase):
     def test_dar_estadiciticas_n_numeros(self):
         self.assertEqual(Estadisticas().dar_estadisticas("1,2,4,3")[0], 4, "N numeros")
         self.assertEqual(Estadisticas().dar_estadisticas("1,2,7,5,6,1")[0], 6, "N numeros")
+
+    def test_dar_estadiciticas_string_vacio_y_minimo(self):
+        self.assertEqual(Estadisticas().dar_estadisticas("")[0], 0, "String vacio")
+        self.assertEqual(Estadisticas().dar_estadisticas("")[1], 0, "Minimo")
