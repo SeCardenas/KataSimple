e060a1d6deab151a58c60148ff5ddfae81674393
dtmaciasca <dt.macias@uniandes.edu.ci>
2019-03-15 22:17:21 -0500

    Iteracion 2. Ciclo 1. Se hace el codigo para un string vacio (Verde)

--- Estadisticas.py
+++ Estadisticas.py
@@ -2,7 +2,7 @@
 class Estadisticas:
     def dar_estadisticas(self, cadena):
         if cadena=="":
-            return [0]
+            return [0,0]
         elif "," in cadena:
             numeros = cadena.split(",")
             return [len(numeros)]
