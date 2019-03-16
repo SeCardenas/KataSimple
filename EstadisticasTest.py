2ce826469eaa7fddead1b79b1603f413efa06460
Sergio Cárdenas <se.cardenas@uniandes.edu.co>
2019-03-15 22:34:51 -0500

    Iteración 2, ciclo 3. Refactor de código para string de dos números (azul)

--- EstadisticasTest.py
+++ EstadisticasTest.py
@@ -3,9 +3,6 @@ import unittest
 from Estadisticas import Estadisticas
 
 class TestEstadisticas(unittest.TestCase):
-    def test_dar_estadiciticas_dos_numeros(self):
-        self.assertEqual(Estadisticas().dar_estadisticas("1,2")[0], 2, "Dos numeros")
-
     def test_dar_estadiciticas_n_numeros(self):
         self.assertEqual(Estadisticas().dar_estadisticas("1,2,4,3")[0], 4, "N numeros")
         self.assertEqual(Estadisticas().dar_estadisticas("1,2,7,5,6,1")[0], 6, "N numeros")
