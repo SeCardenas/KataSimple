b8340a3f6118825d0d00cdb2aedad51bbc62428e
dtmaciasca <dt.macias@uniandes.edu.ci>
2019-03-15 21:49:56 -0500

    Iteracion 1. Ciclo 2. Se hace el test de un string con un numero (Rojo)

--- EstadisticasTest.py
+++ EstadisticasTest.py
@@ -5,3 +5,6 @@ from Estadisticas import Estadisticas
 class TestEstadisticas(unittest.TestCase):
     def test_dar_estadisticas(self):
         self.assertEqual(Estadisticas().dar_estadisticas("")[0], 0, "String vacio")
+
+    def test_dar_estadiciticas_un_solo_numero(self):
+        self.assertEqual(Estadisticas.dar_estadisticas("1")[0], 1, "Un solo numero")
