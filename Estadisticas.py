d554a49a7f9957c3514f84dc2eb5fa7d35c58b93
Sergio Cárdenas <se.cardenas@uniandes.edu.co>
2019-03-15 22:33:59 -0500

    Iteración 2, ciclo 3. Código para string de dos números (verde)

--- Estadisticas.py
+++ Estadisticas.py
@@ -5,6 +5,6 @@ class Estadisticas:
             return [0,0]
         elif "," in cadena:
             numeros = cadena.split(",")
-            return [len(numeros)]
+            return [len(numeros), min(int(numeros[0]), int(numeros[1]))]
         else:
             return [1,int(cadena)]
